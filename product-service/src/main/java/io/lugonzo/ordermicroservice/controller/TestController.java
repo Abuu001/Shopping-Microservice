cd038b02cdd262ae0b7e27a2df4c544f8c35afa9
Abuu001 <abulugz@gmail.com>
2021-09-23 22:38:37 +0300

    test controller

--- product-service/src/main/java/io/lugonzo/ordermicroservice/controller/TestController.java
+++ product-service/src/main/java/io/lugonzo/ordermicroservice/controller/TestController.java
@@ -1,2 +1,21 @@
-package io.lugonzo.ordermicroservice.controller;public class TestController {
+package io.lugonzo.ordermicroservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+@RefreshScope
+public class TestController {
+
+    @Value("${test.name : teesst}")
+    private String name;
+
+    @GetMapping
+    public String test(){
+        return name;
+    }
 }
