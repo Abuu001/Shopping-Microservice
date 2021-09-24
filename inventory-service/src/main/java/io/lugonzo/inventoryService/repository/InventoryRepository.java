01465ab2859e67d9e6f09772a514bfdd14fc612e
Abuu001 <abulugz@gmail.com>
2021-09-24 23:17:25 +0300

    inventory repository

--- inventory-service/src/main/java/io/lugonzo/inventoryService/repository/InventoryRepository.java
+++ inventory-service/src/main/java/io/lugonzo/inventoryService/repository/InventoryRepository.java
@@ -1,2 +1,13 @@
-package io.lugonzo.inventoryService.repository;public interface InventoryRepository {
+package io.lugonzo.inventoryService.repository;
+
+import io.lugonzo.inventoryService.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
