20fccef7ec711d27b1f135e776b2c8f17977beb1
Abuu001 <abulugz@gmail.com>
2021-09-24 23:15:24 +0300

    inventory props

--- inventory-service/src/main/java/io/lugonzo/inventoryService/model/Inventory.java
+++ inventory-service/src/main/java/io/lugonzo/inventoryService/model/Inventory.java
@@ -1,2 +1,22 @@
-package io.lugonzo.inventoryService.model;public class Inventory {
+package io.lugonzo.inventoryService.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "inventory")
+public class Inventory {
+    @Id
+    private Long id;
+    private String skuCode;
+    private Integer stock;
+
 }
