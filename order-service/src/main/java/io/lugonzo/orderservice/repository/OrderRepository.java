903c41296d5c2253b8e71d83d1ae267c55fa0512
Abuu001 <abulugz@gmail.com>
2021-09-24 23:14:33 +0300

    order repository

--- order-service/src/main/java/io/lugonzo/orderservice/repository/OrderRepository.java
+++ order-service/src/main/java/io/lugonzo/orderservice/repository/OrderRepository.java
@@ -4,6 +4,6 @@ import io.lugonzo.orderservice.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/*@Repository
+@Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
-}*/
+}
