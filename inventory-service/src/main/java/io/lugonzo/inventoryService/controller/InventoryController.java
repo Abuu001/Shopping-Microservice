ebaee56f329da04ff94d40437b586f76de30509c
Abuu001 <abulugz@gmail.com>
2021-09-24 23:13:58 +0300

    configured fallback method

--- inventory-service/src/main/java/io/lugonzo/inventoryService/controller/InventoryController.java
+++ inventory-service/src/main/java/io/lugonzo/inventoryService/controller/InventoryController.java
@@ -1,2 +1,26 @@
-package io.lugonzo.inventoryService.controller;public class InventoryController {
+package io.lugonzo.inventoryService.controller;
+
+import io.lugonzo.inventoryService.model.Inventory;
+import io.lugonzo.inventoryService.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryRepository inventoryRepository;
+
+    @GetMapping("/{skuCode}")
+    public Boolean IsInStock(@PathVariable String skuCode){
+
+        Inventory inventory = inventoryRepository.findBySkuCode(skuCode)
+                .orElseThrow(()-> new RuntimeException("cannot find product by sku code " + skuCode));
+
+        return inventory.getStock() > 0;
+    }
 }
