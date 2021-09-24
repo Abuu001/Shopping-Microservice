0e0cabdaa8bd3207631a746a2e3db6f8b3da7eef
Abuu001 <abulugz@gmail.com>
2021-09-24 23:16:22 +0300

    configured openfeign interface

--- order-service/src/main/java/io/lugonzo/orderservice/client/InventoryClient.java
+++ order-service/src/main/java/io/lugonzo/orderservice/client/InventoryClient.java
@@ -1,2 +1,12 @@
-package io.lugonzo.orderservice.client;public interface InventoryClient {
+package io.lugonzo.orderservice.client;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "inventory-service")
+public interface InventoryClient {
+
+    @GetMapping("/api/inventory/{skuCode}")
+    Boolean checkStock(@PathVariable String skuCode);
 }
