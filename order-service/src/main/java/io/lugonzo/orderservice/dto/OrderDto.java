9765426bc28a184ccbcddedcf91531563f572f82
Abuu001 <abulugz@gmail.com>
2021-09-24 23:14:59 +0300

    order props

--- order-service/src/main/java/io/lugonzo/orderservice/dto/OrderDto.java
+++ order-service/src/main/java/io/lugonzo/orderservice/dto/OrderDto.java
@@ -1,2 +1,17 @@
-package io.lugonzo.orderservice.dto;public class OrderDto {
+package io.lugonzo.orderservice.dto;
+
+import io.lugonzo.orderservice.models.OrderLineItems;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDto {
+    private List<OrderLineItems> orderLineItemsList;
 }
--- order-service/src/main/java/io/lugonzo/orderservice/models/Order.java
+++ order-service/src/main/java/io/lugonzo/orderservice/models/Order.java
@@ -1,5 +1,25 @@
 package io.lugonzo.orderservice.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "t_orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItems;
 
 }
